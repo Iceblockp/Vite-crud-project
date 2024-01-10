3ff2eaa9f63a17e520e3b8c5d3a213edd3acb52d
Iceblockp <phonyo126@gmail.com>
2024-01-10 18:38:29 +0630

    for repository

---
+++ src/core/listeners.js
@@ -0,0 +1,12 @@
+import { courseEditFormHandler, courseFormHandler, rowGroupHandler } from "./handlers";
+import { courseEditForm, courseForm, rowGroup } from "./selectors";
+
+const listeners = () => {
+
+    courseForm.addEventListener("submit",courseFormHandler);
+    rowGroup.addEventListener("click",rowGroupHandler);
+    courseEditForm.addEventListener('submit',courseEditFormHandler);
+    
+}
+
+export default listeners;
